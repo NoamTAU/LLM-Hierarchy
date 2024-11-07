3f3683ee9f48602415b34fefa249ce67d5b4c9ed
Daniel James Korchinski <danielkorchinski@gmail.com>
2024-11-07 17:06:25 +0100

    renamed spacy.py -> mask_text.py to avoid clashing with the spacy module.

---
+++ mask_text.py
@@ -0,0 +1,24 @@
+# Command line installs:
+# python -m spacy download en_core_web_sm
+
+
+import spacy
+
+eng_nlp = spacy.load("en_core_web_sm")
+import numpy as np
+import numpy.random
+
+rng = np.random.default_rng(1337)
+
+
+def mask_text(text, rng, probability):
+    doc = eng_nlp(text)
+    new_str = ""
+    mask = "XXXX"
+    for token in doc:
+        # print(token.text)
+        if rng.uniform() < probability and token.text.isalpha():
+            new_str += mask + token.whitespace_
+        else:
+            new_str += token.text_with_ws
+    return new_str
