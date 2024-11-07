66f8c50c44477ec29754d340a6f886bda1110f89
Daniel James Korchinski <danielkorchinski@gmail.com>
2024-11-07 17:09:56 +0100

    updated masking routine to use a specified mask, and to allow the mask_text to pass a different spcay NLP.

--- mask_text.py
+++ mask_text.py
@@ -11,10 +11,11 @@ import numpy.random
 rng = np.random.default_rng(1337)
 
 
-def mask_text(text, rng, probability):
+def mask_text(text, rng, probability,mask="XXXX",eng_nlp=None):
+    if(eng_nlp is None):
+        eng_nlp = spacy.load("en_core_web_sm")
     doc = eng_nlp(text)
     new_str = ""
-    mask = "XXXX"
     for token in doc:
         # print(token.text)
         if rng.uniform() < probability and token.text.isalpha():
