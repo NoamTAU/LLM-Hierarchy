8968f4510813d2e999cb1a327e19648217ec87de
Daniel James Korchinski <danielkorchinski@gmail.com>
2025-01-15 17:39:58 +0100

    added fss datasets without the header, section, subsection dividers.

--- generate_dataset_wiki2_fss.sh
+++ generate_dataset_wiki2_fss.sh
@@ -2,3 +2,4 @@
 
 # This script should be called by `source` command with the correct conda environment.
 python generate_dataset_wiki2.py -n 30 50 70 100 145 190 270 -m 40 60 80 110 155 200 280 -o dataset_wiki_fss
+python generate_dataset_wiki2.py -n 30 50 70 100 145 190 270 -m 40 60 80 110 155 200 280 -o dataset_wiki_fss_decap --strip_headers
